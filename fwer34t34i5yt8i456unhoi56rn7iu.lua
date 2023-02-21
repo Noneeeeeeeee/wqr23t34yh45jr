536ba9acca9b2650eddd7fae39114113c4773191
Nonee <78977500+Noneeeeeeeee@users.noreply.github.com>
2023-02-21 21:30:12 +0100

    Update fwer34t34i5yt8i456unhoi56rn7iu.lua

--- fwer34t34i5yt8i456unhoi56rn7iu.lua
+++ fwer34t34i5yt8i456unhoi56rn7iu.lua
@@ -1,1 +1,46 @@
+local Aerval = loadstring(game:HttpGet('https://raw.githubusercontent.com/Noneeeeeeeee/Gui/main/source'))()
 
+local Window = Aerval:CreateWindow({
+   Name = "Aerval | Universal",
+   LoadingTitle = "Aerval",
+   LoadingSubtitle = "Universal",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "Aerval", 
+      FileName = game.PlaceId
+   },
+   Discord = {
+    Enabled = false,
+    Invite = "pEzCEwe8Wj",
+    RememberJoins = true
+   }
+})
+
+-- Aerval Notification
+
+Aerval:Notify({
+    Title = "Aerval",
+    Content = "Thanks for using our hub.",
+    Duration = 5
+})
+
+-- Main Tab
+
+local Main = Window:CreateTab("Main")
+
+local Main = Main:CreateSection("Main")
+
+-- Player Tab
+
+local Player = Window:CreateTab("Player")
+
+local Player = Player:CreateSection("Player")
+
+-- Credits Tab
+
+local Tab = Window:CreateTab("Credits") 
+local Section = Tab:CreateSection("Credits")
+
+local Label = Tab:CreateLabel("Made by: Nonee")
+local Label = Tab:CreateLabel("Discord: discord.gg/4eYF3Us4Jg7")
+local Label = Tab:CreateLabel("Website: aerval.xyz")
